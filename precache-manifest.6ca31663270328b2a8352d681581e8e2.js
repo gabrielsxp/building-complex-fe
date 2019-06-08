453b627d374478be058e99e778f40a0382341d30
Gabriel Soares <gabrielsxp@hotmail.com>
2019-06-08 19:34:13 -0300

    Updates

---
+++ precache-manifest.6ca31663270328b2a8352d681581e8e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7313a69c15cdf760f19df4e34ae8031b",
+    "url": "/building-complex-fe/index.html"
+  },
+  {
+    "revision": "52b45f8298e0306194f5",
+    "url": "/building-complex-fe/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "21372f5e95cbb7dfea60",
+    "url": "/building-complex-fe/static/js/2.498e9752.chunk.js"
+  },
+  {
+    "revision": "52b45f8298e0306194f5",
+    "url": "/building-complex-fe/static/js/main.8f9970e7.chunk.js"
+  },
+  {
+    "revision": "b0d1d7c38d552e800d96",
+    "url": "/building-complex-fe/static/js/runtime~main.23a23494.js"
+  },
+  {
+    "revision": "04597af908f0ce881b9ce8c4311bcd95",
+    "url": "/building-complex-fe/static/media/b3.04597af9.png"
+  },
+  {
+    "revision": "a853a9b9d691db3524fe54bc5dae76e8",
+    "url": "/building-complex-fe/static/media/herobg.a853a9b9.jpg"
+  }
+]);

5270e80c3e18d5a49cb594acf2b813307e411954
gabrielsxp <gabrielsxp@hotmail.com>
2019-06-08 19:43:34 -0300

    Update servers.js

--- src/constants/servers.js
+++ src/constants/servers.js
@@ -1,7 +1,7 @@
 export const servers = [
-    'http://ec2-3-219-12-16.compute-1.amazonaws.com:3001',
-    'http://ec2-3-217-76-238.compute-1.amazonaws.com:3001',
-    'http://ec2-3-216-163-222.compute-1.amazonaws.com:3001',
-    'http://ec2-3-216-221-195.compute-1.amazonaws.com:3001',
-    'http://ec2-54-158-82-184.compute-1.amazonaws.com:3001'
+    'https://ec2-3-219-12-16.compute-1.amazonaws.com:3001',
+    'https://ec2-3-217-76-238.compute-1.amazonaws.com:3001',
+    'https://ec2-3-216-163-222.compute-1.amazonaws.com:3001',
+    'https://ec2-3-216-221-195.compute-1.amazonaws.com:3001',
+    'https://ec2-54-158-82-184.compute-1.amazonaws.com:3001'
 ];

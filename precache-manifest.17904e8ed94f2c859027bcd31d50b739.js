df3d20d82bd5c3694f2236beac9454c6959a100c
Gabriel Soares <gabrielsxp@hotmail.com>
2019-06-09 14:54:05 -0300

    Updates

--- precache-manifest.17904e8ed94f2c859027bcd31d50b739.js
+++ precache-manifest.17904e8ed94f2c859027bcd31d50b739.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "737f349f354903197c6aa0e5270771b7",
+    "revision": "e0713c340bfc373d0e693e74def484d1",
     "url": "/building-complex-fe/index.html"
   },
   {
-    "revision": "d8d066b66329ee659dfa",
+    "revision": "46ef52d349906277e314",
     "url": "/building-complex-fe/static/css/main.795991f2.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/building-complex-fe/static/js/2.498e9752.chunk.js"
   },
   {
-    "revision": "d8d066b66329ee659dfa",
-    "url": "/building-complex-fe/static/js/main.2566c470.chunk.js"
+    "revision": "46ef52d349906277e314",
+    "url": "/building-complex-fe/static/js/main.37668e89.chunk.js"
   },
   {
     "revision": "b0d1d7c38d552e800d96",

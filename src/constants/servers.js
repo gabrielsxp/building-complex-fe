dacc82e96c89e19c29bb6e1871efa2cceb2c5202
gabrielsxp <gabrielsxp@hotmail.com>
2019-07-18 00:11:40 -0300

    Add files via upload

--- src/constants/servers.js
+++ src/constants/servers.js
@@ -2,14 +2,16 @@
     Esta lista de servidores na próxima entrega do trabalho será dinâmica.
     Para esta etapa, foi apenas utilizada para distribuir as requisições sob os servidores no AWS EC2
 */
-/*
-export const servers = [
-    'http://ec2-3-219-12-16.compute-1.amazonaws.com:3001',
-    'http://ec2-3-217-76-238.compute-1.amazonaws.com:3001',
-    'http://ec2-3-216-163-222.compute-1.amazonaws.com:3001'
-];
-*/
+
+// export const servers = [
+//     'http://ec2-3-219-12-16.compute-1.amazonaws.com:3001',
+//     'http://ec2-3-217-76-238.compute-1.amazonaws.com:3001',
+//     'http://ec2-3-216-163-222.compute-1.amazonaws.com:3001'
+// ];
+
 
 export const servers = [
-    'http://127.0.0.1:3001'
+    'http://ec2-52-90-147-36.compute-1.amazonaws.com:3001',
+    'http://ec2-52-90-147-36.compute-1.amazonaws.com:3001',
+    'http://ec2-52-90-147-36.compute-1.amazonaws.com:3001'
 ];
